1ee2f1146daf47eec70be747608b6d3587ece427
irinapk <irina.park.8@gmail.com>
2021-01-27 03:44:04 +0900

    initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,31 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        String [] names = {"Alex", "Anthony", "Chester", "Kurt"};
+        String [] lastNames = {"Turner", "Kiedes", "Bennington", "Cobain"};
+        Byte [] age = {35, 58, 41, 27};
+
+        for (int i = 0; i < 4; i++) {
+            userService.saveUser(names[i], lastNames[i], age[i]);
+            System.out.println("User с именем " + names[i] + " добавлен в базу данных");
+        }
+
+        List<User> usersList = userService.getAllUsers();
+        System.out.println(usersList.toString());
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
